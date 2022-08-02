b224947ca8a03cdd7df5379870fd901dfe261de2
Anton Ganhammar <ganhammar@gmail.com>
2022-08-02 20:53:36 +0200

    Add active probe count

--- Frontend/Game/features/Map/Object/Planet.js
+++ Frontend/Game/features/Map/Object/Planet.js
@@ -9,7 +9,7 @@ define(() => {
         atmosphereRadius = 90;
         maxRadius = 200;
         minRadius = 50;
-        fuel = Math.ceil(Math.random() * 15) + 5;
+        fuel = Math.ceil(Math.random() * 15) + 8;
         probed = false;
 
         constructor(coordinateX, coordinateY, map) {
