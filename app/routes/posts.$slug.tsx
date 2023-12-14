b9a02734ce9c16467d73dc394f1af7d63107f44b
Anton Ganhammar <ganhammar@gmail.com>
2023-12-14 21:14:59 +0100

    feat: fetch post from ganhammar-posts

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,29 +1,50 @@
 import { json, type LoaderFunction, type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import parseFrontMatter from "front-matter";
+import ReactMarkdown from "react-markdown";
 
 type Post = {
-  slug: string;
   title: string;
   content: string;
+  date: string;
 };
+
+type Attributes = {
+  title: string;
+  id: string;
+  date: string;
+};
+
+const REPOSITORY = "ganhammar/ganhammar-posts";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ name: params.slug, content: "Hello there!" });
+  const token = process.env.API_TOKEN;
+  const response = await fetch(`https://api.github.com/repos/${REPOSITORY}/contents/posts/${params.slug}.mdx`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    return json({ message: "Not found" }, { status: 404 });
+  }
+
+  const data = await response.json();
+  const content = Buffer.from(data.content, "base64").toString();
+
+  const { attributes, body } = parseFrontMatter<Attributes>(content, { allowUnsafe: true });
+
+  return json({ title: attributes.title, content: body });
 };
 
-export const meta: MetaFunction<typeof loader> = ({ data }) => {
+export const meta: MetaFunction<typeof loader> = ({ data: { title } }) => {
   return [{
-    title: `Post ${data.name}`,
+    title,
   }];
 };
 
 export default function Post() {
   let post = useLoaderData() as Post;
 
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </div>
-  );
+  return <ReactMarkdown>{post.content}</ReactMarkdown>;
 }
